0e3bc755e50c9dae99832a12e478a7814a696d78
da.kulikov <den.chernyshev.2014@mail.ru>
2024-01-15 17:59:33 +0300

    del main

--- myself_progs/calculator.py
+++ myself_progs/calculator.py
@@ -1,4 +1,10 @@
 def calculate(x = int(input("Enter the first num: " "")) , y = int(input("Enter the second num: " "")), sign = input("Enter the sign: + or - or * or /: ") ):
+    #####НЕ ДОДЕЛАЛ ИСКЛЮЧЕНИЯ((((
+    try:
+        if x == str():
+            return False, "Try again"
+    except:
+            ValueError
     try:
        if y == 0:
         return False, "Try again"

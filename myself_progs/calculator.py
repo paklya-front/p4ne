a18ec4ea32ad7d2eac58da36faaa95e08f44151b
Den4a <78980302+paklya-front@users.noreply.github.com>
2024-01-16 09:07:16 +0300

    Update calculator.py

--- myself_progs/calculator.py
+++ myself_progs/calculator.py
@@ -1,30 +1,40 @@
-def calculate(x = int(input("Enter the first num: " "")) , y = int(input("Enter the second num: " "")), sign = input("Enter the sign: + or - or * or /: ") ):
-    #####НЕ ДОДЕЛАЛ ИСКЛЮЧЕНИЯ((((
+def calculate():
+
     try:
-        if x == str():
-            return False, "Try again"
-    except:
-            ValueError
-    try:
-       if y == 0:
-        return False, "Try again"
-    except:
-        pass
-    if sign == "+":
-        result = x + y
-        return result
-    elif sign == "-":
-        result = x - y
-        return result
-    elif sign == "*":
-        result = x * y
-        return result
-    elif sign == "/":
-        result = x / y
-        return result
-    else:
-        print("Invalid sign")
-        return 0
+        x = int(input("Enter the first num: " ""))
+        y = int(input("Enter the second num: " ""))
+        sign = input("Enter the sign: + or - or * or /: ")
+        new_x = int(x)
+        new_y = int(y)
+        new_result = 0
+
+
+        if sign == "+":
+            result = x + y
+            return result
+        elif sign == "-":
+            result = x - y
+            return result
+        elif sign == "*":
+            result = x * y
+            return result
+        elif sign == "/":
+            result = x / y
+            return result
+        else:
+            print("Invalid sign")
+            return 0
+            if y == 0 and sign == "/":
+                print("Dont / on zero")
+            if new_x == int(x) or new_y == int(y):
+                return print("Error of calculation")
+
+    except ValueError:
+        print("Invalid input")
+    except ZeroDivisionError:
+        print("Cannot divide by zero")
+    except TypeError:
+        print("Invalid input")
 
 calc = calculate()
 print("Calculating:" "",calc)
